7e058acaa131c6d0c83cccbcdf598c1ae5787eff
ulitsaRaskolnikova <malyshev.2005n@gmail.com>
2024-10-04 14:41:01 +0300

    feat: add find_anagrams function in l2.4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+
+
 fn main() {
-    println!("Hello, world!");
+    let words = vec!["листок", "пятак", "дом", "ток", "тяпка", "слиток", "столик", "кот","пятка", "кто", "флекс"];
+    
+    let anagrams = find_anagrams(&words);
+    
+    println!("{:?}", anagrams);
+}
+
+fn find_anagrams(words: &[&str]) -> HashMap<String, Vec<String>> {
+    let mut anagram_map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for word in words {
+        let word = word.to_lowercase();
+        let mut sorted_word: Vec<char> = word.chars().collect();
+        sorted_word.sort();
+        let key = sorted_word.iter().collect::<String>();
+        anagram_map.entry(key).or_insert(Vec::new()).push(word.to_string());
+    }
+    
+    let mut result_map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for mut anagrams in anagram_map.into_values() {
+        if anagrams.len() > 1 {
+            anagrams.sort();
+            result_map.insert(anagrams[0].clone(), anagrams);
+        }
+    }
+
+    result_map
 }
